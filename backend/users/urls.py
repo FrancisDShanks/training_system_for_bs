e3589f7fc0f151892607bda371cf4c41e12d63f5
francis <duxufan@beyondsoft.com>
2019-04-24 09:19:35 +0800

    4/24

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, re_path, include
 
 
-
+'''
 urlpatterns += [
     path('apt-auth/', include('rest_framework.urls')),
 ]
+'''
