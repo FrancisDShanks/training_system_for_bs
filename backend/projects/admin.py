b8b7d67370f2e8f495373552a5592469be769b2c
francis <duxufan@beyondsoft.com>
2019-07-15 17:42:46 +0800

    update gitignore

--- backend/projects/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Activity, Material
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(Activity)
-admin.site.register(Material)
--- backend/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Organization
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Organization)
