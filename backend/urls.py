da66358d55fe91b20313778379f434b98db3674e
francis <duxufan@beyondsoft.com>
2019-07-23 09:47:28 +0800

    add tools dir

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,7 @@ router.register(r'materials', projects_view.MaterialViewSet)
 urlpatterns = [
 
     path('', include(router.urls)),
-    path('apt-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
 '''
