1a73e63cded257f8d66573d337e0e06b91ece62e
francis <duxufan@beyondsoft.com>
2019-07-23 10:43:22 +0800

    add phone verifycode

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,15 +6,16 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     # TODO: default for dev
-    chinese_name = models.CharField(verbose_name=u'中文名', max_length=100, null=False)
-    english_name = models.CharField(verbose_name=u'英文名', max_length=100, blank=True)
+    chinese_name = models.CharField(verbose_name=u'中文名', max_length=30, null=False)
+    english_name = models.CharField(verbose_name=u'英文名', max_length=30, blank=True, null=True)
     created_time = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True)
     # TODO: check doc for auto_now
     last_seen = models.DateTimeField(verbose_name=u'更新时间', auto_now=True)
     company_id = models.IntegerField(verbose_name=u'员工号', unique=True)
     position = models.CharField(verbose_name=u'职位', max_length=100, blank=True)
+    gender = models.CharField(verbose_name=u'性别', max_length=6, choices=(('male', '男'), ('female', '女')), default='male', help_text='性别')
     email = models.EmailField(verbose_name=u'电子邮件地址', max_length=100, unique=True)
-    phone = models.PositiveIntegerField(verbose_name=u'手机号', blank=True, unique=True)
+    phone = models.CharField(verbose_name=u'手机号码', max_length=20, help_text='手机号码')
     department = models.CharField(verbose_name=u'部门', max_length=100, blank=True, default='ISV')
     organization = models.ForeignKey('Organization', verbose_name=u'组织名称', on_delete=models.SET_NULL,
                                      related_name='user_profile', null=True)
@@ -43,5 +44,20 @@ class Organization(models.Model):
     def __str__(self):
         return '<Organization> ID:{0} Name:{1}'.format(self.id, self.name)
 
+    def __repr__(self):
+        return self.__str__()
+
+
+class VerifyCode(models.Model):
+    code = models.CharField(verbose_name=u'验证码', max_length=20, help_text='验证码')
+    mobile = models.CharField(verbose_name=u'手机号码', max_length=20, help_text='手机号码')
+    add_time = models.DateTimeField(verbose_name=u'添加时间', auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = verbose_name = ''
+
+    def __str__(self):
+        return '<Phone_VerifyCode> code:{0} mobile:{1}'.format(self.code, self.mobile)
+    
     def __repr__(self):
         return self.__str__()
